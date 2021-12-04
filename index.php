d23b97bc2095a03aebeb30abca2843a238b9f1f6
Mohammed <johnjohny90@hotmail.com>
2021-12-04 23:06:09 +0100

    actualizar todos los archivos

--- index.php
+++ index.php
@@ -367,7 +367,16 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
+					    
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+
+						<h1>Fecha y hora</h1>
+						<p><?php
+                         date_default_timezone_set('Europe/Madrid');    
+                         $Date = date('d/m/Y H:i:s');  
+                         echo "La fecha actual es: $Date";
+                         ?></p>
+
                         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                     </div>
